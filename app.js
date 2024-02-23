aa65b8f4d1433c8601a01e203cac352b55b06e34
Manveer Sohal <marzveer123@gmail.com>
2024-02-23 02:45:20 -0500

    commented app.js and pic.html, added buttons and input text field to enter text, added sending text to server and entering to database, added getting text from database and displaying it on the back of each picture

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const path = require('path');
-const axios = require('axios');
 const multer = require('multer');
 const fs = require('fs');
+const mysql = require('mysql2')
 
 const app = express();
 const port = 3000;
+
 const upload = multer({ dest: 'uploads/' });
-
-
-
 app.use(express.json({limit:'1mb'}));
 
-const mysql = require('mysql2')
 
+//creats the pool connection to mysql databas
 const pool = mysql.createPool({
     user: 'root',
     host: 'localhost',
@@ -24,12 +22,18 @@ const pool = mysql.createPool({
 
 
 
-//get image data
+//fetches image file url from pic.html, and puts the binary into database
 app.post('/api',  upload.single('file'),  (req,res) =>{
+
+
     const filePath = req.file.path;
     const fileData = fs.readFileSync(filePath);
-
-    const result = pool.query('INSERT INTO photos (photoName, photoText, photoData) VALUES ("pic1", "Manu and Noshie turned into paper!", ?)', [fileData]);
+    const fileText = req.body.text;
+    //puts the photo into database
+    const result = pool.query('INSERT INTO photos (photoName, photoText, photoData) VALUES ("pic1", ?, ?)', [fileText, fileData]);
+   
+    //gets the data back, temp code
+    /*
     try{
     const results =  pool.query('SELECT photoData FROM photos WHERE id = 9')
     .then(results => {
@@ -50,45 +54,53 @@ app.post('/api',  upload.single('file'),  (req,res) =>{
             console.error('Error retrieving photo from database:', error);
             res.status(500).send('Error retrieving photo from database');
         }       
-
+   */
     });
 
 
+
+    //gets all the data in database, converts the binary into url form and sends it back
     app.post('/load', (req,res) =>{
         console.log("loading");
+
         try{
         const results =  pool.query('SELECT * FROM photos')
         .then(results => {
-    
+            
                 if(results && results.length > 0){
+
                     const imageData=[];
                     const imageUrl = [];
+                    const imageText = [];
+
                     for(var i = 0; i < results[0].length; i++){
+                        imageText[i] = results[0][i].photoText;
                         imageData[i] =  results[0][i].photoData.toString('base64');
                         imageUrl[i]= `data:image/jpeg;base64,${imageData[i]}`;
                         
                     }
                 
-                    res.json({
-                        url:imageUrl
+                    res.json(
+                    {
+                        url:imageUrl,
+                        text:imageText
                     });
 
                 }
                 else {
+
                     console.log('No record found ');
                 }
             })
             } catch (error) {
+
                 console.error('Error retrieving photo from database:', error);
                 res.status(500).send('Error retrieving photo from database');
-            }    
-    
-       
+            }   
     
         });
 
 
-let photoCount = 0;
 
 //direcotry to html 
 const publicPath = path.join(__dirname , '/public');
